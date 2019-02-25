fc1818ef3ef63958d586306e3f04f91e75cf5b67
yevheniia <jene4ka1818@gmail.com>
2019-02-25 11:27:01 -0500

    final

--- assets/java/game.js
+++ assets/java/game.js
@@ -5,10 +5,10 @@ var maxGuess = 10;
 var storeArr = [];
 var guessedLetters = [];
 var remainingGuesses = 0;
-var numWins = 0;
-var numLosses = 0;
-var theEnd = false;
-var newWord;
+var winsCount = 0;
+var losseCount = 0;
+var gameOver = false;
+var randomWord;
 
 var image1 = '../WordGuessGame/assets/images/greeniguana.jpeg';
 var image2 = '../WordGuessGame/assets/images/crested_gecko.jpg';
@@ -22,12 +22,13 @@ var image9 = '../WordGuessGame/assets/images/monitor.jpg';
 var image10 = '../WordGuessGame/assets/images/kingcobra.jpg';
 var image11 = '../WordGuessGame/assets/images/copperhead.jpg';
 var image12 = '../WordGuessGame/assets/images/boa.jpg';
+var imgGameOver = '../WordGuessGame/assests/images/videoblocks-game-over-8-bit-funky-a-funky-colorful-4k-game-over-screen-animation-letters-falling-towards-the-center-8-bit-retro-style-red-and-yellow_hcqx9_kax_thumbnail-full06.png'
 
 function start() {
-    newWord = reptileTypes[Math.floor(Math.random() * reptileTypes.length)];
+    randomWord = reptileTypes[Math.floor(Math.random() * reptileTypes.length)];
     storeArr = [];
     
-    for (var i =0; i < newWord.length; i++) {
+    for (var i =0; i < randomWord.length; i++) {
         storeArr[i] = "_";
     }
 
@@ -38,13 +39,13 @@ function start() {
     document.getElementById("image").src = "";
     document.getElementById('numberOfGuesses').style.color = "";
     document.getElementById('button1').style.visibility = "hidden";
-
-    updateFunc();
+   
+    refresh();
 };
 
-function updateFunc(){
-    document.getElementById("numberWins").innerHTML = numWins;
-    document.getElementById("numberLosses").innerHTML = numLosses;
+function refresh(){
+    document.getElementById("numberWins").innerHTML = winsCount;
+    document.getElementById("numberLosses").innerHTML = losseCount;
     document.getElementById("numberOfGuesses").innerHTML = remainingGuesses;
     document.getElementById("answerWord").innerHTML = storeArr.join("");
     document.getElementById("guessedLetters").innerHTML = guessedLetters;
@@ -54,14 +55,14 @@ function checkfunc(letter) {
     if (guessedLetters.indexOf(letter)=== -1){
         guessedLetters.push(letter);
 
-        if (newWord.indexOf(letter) === -1){
+        if (randomWord.indexOf(letter) === -1){
             remainingGuesses--;
             if (remainingGuesses <=3) {
                 document.getElementById("numberOfGuesses").style.color = "#e12d2e";
             }
         }else{
-            for (var i = 0; i < newWord.length; i++){
-                if (letter === newWord[i]){
+            for (var i = 0; i < randomWord.length; i++){
+                if (letter === randomWord[i]){
                     storeArr[i] = letter;
                 }
             }
@@ -70,55 +71,55 @@ function checkfunc(letter) {
 };
 
 function winFunc(){
-    if (storeArr.indexOf("_")=== -1){
-        numWins++;
-        theEnd = true;
+    if (storeArr.indexOf("_") === -1){
+        winsCount++;
+        gameOver = true;
 
-        if(newWord === "IGUANA"){
+        if(randomWord === "IGUANA"){
             document.getElementById("image").src = image1;
             document.getElementById("button1").style.visibility = "visible";
             document.getElementById("button1").href = "https://youtu.be/NC7TLR0tBPU";
-        }else if (newWord === "GECKO"){
+        }else if (randomWord === "GECKO"){
             document.getElementById("button1").style.visibility = "visible";
             document.getElementById("button1").href = "https://youtu.be/KwWzQMkEmP4";
             document.getElementById("image").src = image2;
-        }else if (newWord === "POGONA"){
+        }else if (randomWord === "POGONA"){
             document.getElementById("image").src = image3;
             document.getElementById("button1").style.visibility = "visible";
             document.getElementById("button1").href = "https://www.youtube.com/watch?v=FJUfpQMBoFY&t=370s";
-        }else if (newWord === "CAIMANLIZARD"){
+        }else if (randomWord === "CAIMANLIZARD"){
             document.getElementById("image").src = image4;
             document.getElementById("button1").style.visibility = "visible";
             document.getElementById("button1").href = "https://youtu.be/NC7TLR0tBPU";
-        }else if (newWord === "BALLPYTHON"){
+        }else if (randomWord === "BALLPYTHON"){
                 document.getElementById("image").src = image5;
                 document.getElementById("button1").style.visibility = "visible";
             document.getElementById("button1").href = "https://youtu.be/HMyxhKT0CQQ";
-        }else if (newWord === "TREEFROG"){
+        }else if (randomWord === "TREEFROG"){
             document.getElementById("image").src = image8;
             document.getElementById("button1").style.visibility = "visible";
             document.getElementById("button1").href = "https://youtu.be/jly9jgOqrzI";
-        }else if (newWord === "ANOLE"){
+        }else if (randomWord === "ANOLE"){
             document.getElementById("image").src = image7;
             document.getElementById("button1").style.visibility = "visible";
             document.getElementById("button1").href = "https://youtu.be/blXmYtPXrQ4";
-        }else if (newWord === "CORNSNAKE"){
+        }else if (randomWord === "CORNSNAKE"){
             document.getElementById("image").src = image6;
             document.getElementById("button1").style.visibility = "visible";
             document.getElementById("button1").href = "https://youtu.be/vjQ8e84JHrA";
-        }else if (newWord === "MONITOR"){
+        }else if (randomWord === "MONITOR"){
             document.getElementById("image").src = image9;
             document.getElementById("button1").style.visibility = "visible";
             document.getElementById("button1").href = "https://youtu.be/4zqj4h8kx2M";
-        }else if (newWord === "KINGCOBRA"){
+        }else if (randomWord === "KINGCOBRA"){
             document.getElementById("image").src = image10;
             document.getElementById("button1").style.visibility = "visible";
             document.getElementById("button1").href = "https://youtu.be/GCXzQ1fsbN8";
-        }else if (newWord === "COPPERHEAD"){
+        }else if (randomWord === "COPPERHEAD"){
             document.getElementById("image").src = image11;
             document.getElementById("button1").style.visibility = "visible";
             document.getElementById("button1").href = "https://youtu.be/N8cqUB1HBqU";
-        }else if (newWord === "CONSTRICTOR"){
+        }else if (randomWord === "CONSTRICTOR"){
             document.getElementById("image").src = image12;
             document.getElementById("button1").style.visibility = "visible";
             document.getElementById("button1").href = "https://youtu.be/bACJQZG4b40";
@@ -129,21 +130,21 @@ function winFunc(){
 
 function losseFunc(){
     if (remainingGuesses <=0){
-        numLosses++;
-        theEnd = true;
-        alert("oh no.......try again!");
+        losseCount++;
+        gameOver = true;
+        $(".body").append(imgGameOver)
     }
 };
 
 document.onkeyup = function(event) {
 
-    if (theEnd){
+    if (gameOver){
         start();
-        theEnd = false;
+        gameOver = false;
     }else{
         if(event.keyCode >= 65 && event.keyCode <=90){
             checkfunc(event.key.toUpperCase());
-            updateFunc();
+            refresh();
             winFunc();
             losseFunc();
         }
@@ -151,6 +152,6 @@ document.onkeyup = function(event) {
 };
 
 start();
-updateFunc();
+refresh();
 
-console.log(newWord);
+console.log(randomWord);
